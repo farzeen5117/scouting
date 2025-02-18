5658f0b7e5c9968463d7adbc48322097500fb559
farzeen <farzeen@FARZEEN-LAPTOP>
2025-02-18 14:41:28 -0600

    added barge time stopwatch

--- lib/generate_qr_page.dart
+++ lib/generate_qr_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'qr_image.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
 class GenerateQRCode extends StatefulWidget {
   const GenerateQRCode({super.key});
@@ -27,6 +28,8 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
     TextEditingController(), // 13 - Teleop Coral L4
     TextEditingController(), // 14 - Teleop Processor Score
     TextEditingController(), // 15 - Teleop Net Score
+    TextEditingController(), // 16 - Scored in Opponent Processor
+    TextEditingController(), // 17 - Barge Time
   ];
   TextEditingController finalController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
@@ -39,6 +42,40 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
     'Deep Cage'
   ];
 
+  Timer? _timer;
+  int _start = 0;
+
+  void _startStopwatch() {
+    _start = 0;
+    _timer = Timer.periodic(const Duration(milliseconds: 1), (timer) {
+      setState(() {
+        _start++;
+        controllers[17].text = _formatTime(_start);
+      });
+    });
+  }
+
+  void _stopStopwatch() {
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+  }
+
+  void _resetStopwatch() {
+    _timer?.cancel();
+    setState(() {
+      _start = 0;
+      controllers[17].text = _formatTime(_start);
+    });
+  }
+
+  String _formatTime(int milliseconds) {
+    final int minutes = milliseconds ~/ 60000;
+    final int remainingSeconds = (milliseconds % 60) ~/ 1000;
+    final int remainingMilliseconds = milliseconds % 1000;
+    return '${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}:${remainingMilliseconds.toString().padLeft(3, '0')}';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -47,19 +84,23 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
+            // Scouter's Name
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[0],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               decoration: const InputDecoration(
                   border: OutlineInputBorder(), labelText: 'Scouter\'s Name'),
             ),
           ),
           Container(
+            // Match Number
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[1],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [
                 FilteringTextInputFormatter.digitsOnly,
@@ -70,10 +111,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Team Number
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[2],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [
                 FilteringTextInputFormatter.digitsOnly,
@@ -84,22 +127,25 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Auton Section
             margin: const EdgeInsets.all(20),
-            alignment: Alignment.centerLeft,
+            alignment: Alignment.center,
             child: Text(
               'Auton:',
               style: const TextStyle(
                 fontSize: 25,
                 color: Colors.black,
               ),
-              textAlign: TextAlign.left,
+              textAlign: TextAlign.center,
             ),
           ),
           Container(
+            // Leave Starting Line
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[3],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               inputFormatters: [LengthLimitingTextInputFormatter(3)],
               decoration: const InputDecoration(
                   border: OutlineInputBorder(),
@@ -107,10 +153,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Auton Coral L1
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[4],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -118,10 +166,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Auton Coral L2
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[5],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -129,10 +179,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Auton Coral L3
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[6],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -140,10 +192,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Auton Coral L4
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[7],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -151,10 +205,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Auton Processor Score
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[8],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -162,10 +218,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Auton Net Score
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[9],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -173,22 +231,25 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Teleop Section
             margin: const EdgeInsets.all(20),
-            alignment: Alignment.centerLeft,
+            alignment: Alignment.center,
             child: Text(
               'Teleop:',
               style: const TextStyle(
                 fontSize: 25,
                 color: Colors.black,
               ),
-              textAlign: TextAlign.left,
+              textAlign: TextAlign.center,
             ),
           ),
           Container(
+            // Teleop Coral L1
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[10],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -196,10 +257,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Teleop Coral L2
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[11],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -207,10 +270,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Teleop Coral L3
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[12],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -218,10 +283,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Teleop Coral L4
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[13],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -229,10 +296,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Teleop Processor Score
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[14],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -240,10 +309,12 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
+            // Teleop Net Score
             margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controllers[15],
               cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
               keyboardType: TextInputType.number,
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               decoration: const InputDecoration(
@@ -251,12 +322,15 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
             ),
           ),
           Container(
-              margin: const EdgeInsets.all(20),
+              // Pickup From Dropdown
+              margin: const EdgeInsets.all(25),
+              alignment: Alignment.centerLeft,
               child: DropdownButton(
                 value: dropdownValue,
                 icon: const Icon(Icons.arrow_drop_down),
                 elevation: 16,
                 style: const TextStyle(color: Colors.black),
+                dropdownColor: Colors.white,
                 underline: Container(
                   height: 2,
                   color: Colors.black,
@@ -273,10 +347,100 @@ class GenerateQRCodeState extends State<GenerateQRCode> {
                   );
                 }).toList(),
               )),
+          Container(
+            // Scored in Opponent Processor
+            margin: const EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            child: TextField(
+              controller: controllers[16],
+              cursorColor: Colors.black,
+              style: const TextStyle(color: Colors.black),
+              inputFormatters: [LengthLimitingTextInputFormatter(3)],
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Scored in Opponent Processor (yes/no)'),
+            ),
+          ),
+          Container(
+            // Endgame Section
+            margin: const EdgeInsets.all(20),
+            alignment: Alignment.center,
+            child: Text(
+              'Endgame:',
+              style: const TextStyle(
+                fontSize: 25,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            // Barge Time Stopwatch
+            margin: const EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: controllers[17],
+                    cursorColor: Colors.black,
+                    style: const TextStyle(color: Colors.black),
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Barge Time'),
+                  ),
+                ),
+                IconButton(
+                    icon: const Icon(Icons.play_arrow, color: Colors.black),
+                    onPressed: () {
+                      _startStopwatch();
+                    }),
+                IconButton(
+                    icon: const Icon(Icons.stop, color: Colors.black),
+                    onPressed: () {
+                      _stopStopwatch();
+                    }),
+                IconButton(
+                    icon: const Icon(Icons.replay, color: Colors.black),
+                    onPressed: () {
+                      _resetStopwatch();
+                    }),
+              ],
+            ),
+          ),
+          Container(
+              // Robot Status Dropdown
+              margin: const EdgeInsets.all(25),
+              alignment: Alignment.centerLeft,
+              child: DropdownButton(
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_drop_down),
+                elevation: 16,
+                style: const TextStyle(color: Colors.black),
+                dropdownColor: Colors.white,
+                underline: Container(
+                  height: 2,
+                  color: Colors.black,
+                ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
+                },
+                items: finalRobotStatus
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              )),
           //This button when pressed navigates to QR code generation
           ElevatedButton(
+              // TODO: change button color
               onPressed: () async {
-                finalController.text =
+                finalController
+                        .text = // TODO: add everything else to finalController
                     '${controllers[0].text}\t${controllers[1].text}\t${controllers[2].text}\t${controllers[3].text}\t${controllers[4].text}\t${controllers[5].text}\t${controllers[6].text}\t${controllers[7].text}\t${controllers[8].text}\t${controllers[9].text}\t${controllers[10].text}\t${controllers[11].text}\t${controllers[12].text}\t${controllers[13].text}\t${controllers[14].text}\t${controllers[15].text}';
                 ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
